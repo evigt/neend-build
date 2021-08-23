967cc0e858fd740dad357ad9ffd1f8aa77caa3e6
yashseh <1921900.bca.cbsa@cgc.edu.in>
2021-08-23 15:22:57 +0530

    cummit

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,12 +3,15 @@ import copy from '../../images/copy.svg';
 const year = new Date().getFullYear();
 
 const Footer = () => {
-return(
-    <> 
-    <div className="container">
-    <p id="footer"><img style={{marginBottom:"3px"}} src={copy} alt="copy"/> Neend <span>{year}</span>. All Rights Reserved.</p>
-    </div>
+  return (
+    <>
+      <div className="container">
+        <p id="footer">
+          <img style={{ marginBottom: '3px' }} src={copy} alt="copy" /> Neend{' '}
+          <span>{year}</span>. All Rights Reserved.
+        </p>
+      </div>
     </>
-) 
-}
+  );
+};
 export default Footer;
