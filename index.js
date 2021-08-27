98d43340239a08aa19a2e1fec07ddc026031a4a3
yashseh <79434749+yashseh@users.noreply.github.com>
2021-08-28 01:08:20 +0530

    New update

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import App from './App';
+
+ReactDOM.render(
+
+  <App />
+,
+  document.getElementById('root')
+);
+
